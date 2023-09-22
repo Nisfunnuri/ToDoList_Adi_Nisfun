17bb417855067c8830393d604782bf57f5aa1585
Nisfunnuri <mnisfunnuri@gmail.com>
2023-09-22 14:21:22 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,27 @@
+/**
+ * ToDoList
+ * Main feature : 
+ * 
+//  * ADI
+
+- Menambah data todo, yang perlu disimpan :
+    - id
+    - todo
+    - date (format : “yyyy-mm-dd”)
+    - isDone
+
+- Menampilkan list data berdasarkan todo yang telah selesai atau yang belum selesai
+
+- Memperbarui data todo, yang bisa diperbarui :
+    - todo
+    - date
+    - isDone
+
+
+//  Nisfun
+- Menampilkan list data todo
+
+- Menampilkan data berdasarkan rentang tanggal mulai dan akhir yang diberikan
+
+- Menghapus data todo yang dipilih
+ */
